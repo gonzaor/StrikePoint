f0e4099a06cc21bab9d99a8172d9e75860b01ee6
Gonzalo Or <95729955+gonzaor@users.noreply.github.com>
2023-08-28 10:21:03 -0300

    Add files via upload
    Se agrego el index.

---
+++ Vista/app.js
@@ -0,0 +1,32 @@
+var TIMEOUT = 6000;
+
+
+var interval = setInterval(handleNext, TIMEOUT);
+
+function handleNext() {
+
+  var $radios = $('input[class*="slide-radio"]');
+  var $activeRadio = $('input[class*="slide-radio"]:checked');
+
+  var currentIndex = $activeRadio.index();
+  var radiosLength = $radios.length;
+
+  $radios
+    .attr('checked', false);
+
+
+  if (currentIndex >= radiosLength - 1) {
+
+    $radios
+      .first()
+      .attr('checked', true);
+
+  } else {
+
+    $activeRadio
+      .next('input[class*="slide-radio"]')
+      .attr('checked', true);
+
+  }
+
+}

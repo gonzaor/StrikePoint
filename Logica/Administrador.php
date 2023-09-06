6c759b82bdc23ac9aced9105f2e359a8df99c62b
Gonzalo <95729955+gonzaor@users.noreply.github.com>
2023-09-06 06:48:35 -0300

    Se creo adminBD, se agrego logica al index.

---
+++ Logica/Administrador.php
@@ -0,0 +1,105 @@
+<?php
+
+class Administrador 
+{
+
+/*/*Base de datos CREATE TABLE Admin (
+	ci_admin INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(30) NOT NULL,
+	apellido VARCHAR(30) NOT NULL,
+	contraseña VARCHAR(40) NOT NULL,
+	estado VARCHAR(15) NOT NULL,
+	mail VARCHAR(30) NOT NULL,
+	telefono INT NOT NULL
+);
+*/ 
+
+    private $ci_admin;
+    private $nombre;
+    private $apellido;
+    private $contraseña;
+    private $estado;
+    private $mail;
+    private $telefono;
+
+    public function getCIAdmin() {
+        return $this->ci_admin;
+    }
+
+    public function setCIAdmin($ci_admin) {
+        $this->ci_admin = $ci_admin;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellido() {
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido) {
+        $this->apellido = $apellido;
+    }
+
+    public function getContraseña() {
+        return $this->contraseña;
+    }
+
+    public function setContraseña($contraseña) {
+        $this->contraseña = $contraseña;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    public function getMail() {
+        return $this->mail;
+    }
+
+    public function setMail($mail) {
+        $this->mail = $mail;
+    }
+
+    public function getTelefono() {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+    public function registrarAdmin() {
+        include_once("Persistencia/AdminBD.php");
+        $adminBD = new AdminBD();
+        return $adminBD->registrarAdmin($this->ci_admin, $this->nombre, $this->apellido, $this->contraseña, $this->estado, $this->mail, $this->telefono);
+    }
+
+    public function iniciarSesion() {
+        include_once("Persistencia/AdminBD.php");
+        $adminBD = new AdminBD();
+        return $adminBD->IniciarSesion($this->ci_admin, $this->contraseña);
+    }
+
+    public function listarAdmin() {
+        include_once("Persistencia/AdminBD.php");
+        $adminBD = new AdminBD();
+        return $adminBD->listarAdmin();
+    }
+
+    public function eliminarAdmin() {
+        include_once("Persistencia/AdminBD.php");
+        $adminBD = new AdminBD();
+        return $adminBD->eliminarAdmin($this->ci_admin);
+    }
+
+
+}
---
+++ Persistencia/AdminBD.php
@@ -0,0 +1,84 @@
+<?php
+
+class AdminBD extends Conexion
+{
+
+
+/*Base de datos CREATE TABLE Admin (
+	ci_admin INT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(30) NOT NULL,
+	apellido VARCHAR(30) NOT NULL,
+	contraseña VARCHAR(40) NOT NULL,
+	estado VARCHAR(15) NOT NULL,
+	mail VARCHAR(30) NOT NULL,
+	telefono INT NOT NULL
+);
+*/
+
+	public function registrarAdmin($ci_admin, $nombre, $apellido, $contraseña, $estado, $mail, $telefono) {
+		$db = $this->conectar();
+		$stmt = $db->prepare("INSERT INTO Admin (ci_admin, nombre, apellido, contraseña, estado, mail, telefono) VALUES (?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isssssi", $ci_admin, $nombre, $apellido, $contraseña, "Activo", $mail, $telefono);
+		if($stmt->execute()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function IniciarSesion($ci_admin, $contraseña) {
+		$db = $this->conectar();
+		$stmt = $db->prepare("SELECT * FROM Admin WHERE ci_admin = ? AND contraseña = ?");
+		$stmt->bind_param("is", $ci_admin, $contraseña);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if($result->num_rows > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+    public function modificarAdmin($ci_admin, $nombre, $apellido, $contraseña, $estado, $mail, $telefono) {
+   		$db = $this->conectar();
+   		$stmt = $db->prepare("UPDATE Admin SET nombre = ?, apellido = ?, contraseña = ?, estado = ?, mail = ?, telefono = ? WHERE ci_admin = ?");
+   		$stmt->bind_param("ssssssi", $nombre, $apellido, $contraseña, $estado, $mail, $telefono, $ci_admin);
+   		if($stmt->execute()) {
+   			return true;
+   		} else {
+   			return false;
+   		}
+   	}
+    
+    public function eliminarAdmin($ci_admin) {
+        $db = $this->conectar();
+        $stmt = $db->prepare("UPDATE Admin SET estado = 'F' WHERE ci_admin = ?");
+        $stmt->bind_param("i", $ci_admin);
+        if($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+
+
+    public function listarAdmin() {
+   		$db = $this->conectar();
+   		$stmt = $db->prepare("SELECT * FROM Admin");
+   		$stmt->execute();
+   		$result = $stmt->get_result();
+   		$admins = array();
+   		while($row = $result->fetch_assoc()) {
+   			$admins[] = $row;
+   		}
+   		return $admins;
+   	}
+
+ 
+    
+    
+
+
+
+}
